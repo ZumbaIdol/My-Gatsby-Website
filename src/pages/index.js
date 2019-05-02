0b19150fdfc7e57c519c2444a688b2ef7035514d
David A. Foote <david.a.foote@gmail.com>
2019-05-02 00:14:30 -0500

    Clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,6 @@ const IndexPage = () => {
       <Head title="Home"/>
       <h1>Hello.</h1>
       <h2>I'm David, a full-stack developer living in beautiful Saint Louis.</h2>
-      <p>Need a developer?  <Link to="/contact">Contact me</Link></p>
     </Layout>
   )
 }
